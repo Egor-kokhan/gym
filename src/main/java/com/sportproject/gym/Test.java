16e5607d20739c75429009b4758d2aa027bfe444
Alexandr <Sanom1907@gmail.com>
2020-05-07 19:40:24 +0300

    Yop

--- src/main/java/com/sportproject/gym/Test.java
+++ src/main/java/com/sportproject/gym/Test.java
@@ -8,7 +8,7 @@ import java.util.Iterator;
 public class Test {
     public static void main(String[] args) {
         HashMap<String, String> map = new HashMap<>();
-        map.put("1", "1");
+        map.put("11", "11");
         map.put("2", "2");
         map.put("3", "3");
 
