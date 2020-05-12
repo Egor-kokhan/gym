a7a42a38b29b28437a57b0bed1c526901027f45f
Egor <e.kohan@itlc.ru>
2020-05-12 20:33:38 +0300

    begin project structure

---
+++ src/main/java/com/sportproject/gym/service/PersonServiceImpl.java
@@ -0,0 +1,38 @@
+package com.sportproject.gym.service;
+
+import com.sportproject.gym.entity.Person;
+import com.sportproject.gym.repository.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * @author Egor on 12.05.2020.
+ */
+@Service
+public class PersonServiceImpl implements PersonService{
+
+    private PersonRepository repository;
+
+    @Autowired
+    public PersonServiceImpl(PersonRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public List<Person> getAll() {
+        return repository.findAll();
+    }
+
+    @Override
+    public List<Person> getAllMoreOlder() {
+        List<Person> persons = repository.findAll();
+        for (Person person : persons) {
+            person.setAge(person.getAge()+5);
+        }
+
+        return persons;
+    }
+
+}
