c991148ebb0489bb3cfcbd3aa55622b5b3f07859
Egor <e.kohan@itlc.ru>
2020-05-20 19:45:35 +0300

    added sequence

--- src/main/java/com/sportproject/gym/entity/Gymnastic.java
+++ src/main/java/com/sportproject/gym/entity/Gymnastic.java
@@ -1,15 +1,20 @@
 package com.sportproject.gym.entity;
 
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+
 import javax.persistence.*;
 import java.util.Set;
 
 /**
  * @author Alex on 14.05.2020.
  */
+@Data
 @Entity()
 public class Gymnastic {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(nullable = false)
@@ -18,79 +23,9 @@ public class Gymnastic {
     @Column(nullable = false)
     private String description;
 
+    @ToString.Exclude
+    @EqualsAndHashCode.Exclude
     @ManyToMany(mappedBy = "gymnastics", fetch = FetchType.LAZY)
     private Set<Person> persons;
 
-    public Gymnastic() {
-    }
-
-    public Long getId() {
-        return this.id;
-    }
-
-    public String getName() {
-        return this.name;
-    }
-
-    public String getDescription() {
-        return this.description;
-    }
-
-    public Set<Person> getPersons() {
-        return this.persons;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public void setPersons(Set<Person> persons) {
-        this.persons = persons;
-    }
-
-    public boolean equals(final Object o) {
-        if (o == this) return true;
-        if (!(o instanceof Gymnastic)) return false;
-        final Gymnastic other = (Gymnastic) o;
-        if (!other.canEqual((Object) this)) return false;
-        final Object this$id = this.getId();
-        final Object other$id = other.getId();
-        if (this$id == null ? other$id != null : !this$id.equals(other$id)) return false;
-        final Object this$name = this.getName();
-        final Object other$name = other.getName();
-        if (this$name == null ? other$name != null : !this$name.equals(other$name)) return false;
-        final Object this$description = this.getDescription();
-        final Object other$description = other.getDescription();
-        if (this$description == null ? other$description != null : !this$description.equals(other$description))
-            return false;
-        return true;
-    }
-
-    protected boolean canEqual(final Object other) {
-        return other instanceof Gymnastic;
-    }
-
-    public int hashCode() {
-        final int PRIME = 59;
-        int result = 1;
-        final Object $id = this.getId();
-        result = result * PRIME + ($id == null ? 43 : $id.hashCode());
-        final Object $name = this.getName();
-        result = result * PRIME + ($name == null ? 43 : $name.hashCode());
-        final Object $description = this.getDescription();
-        result = result * PRIME + ($description == null ? 43 : $description.hashCode());
-        return result;
-    }
-
-    public String toString() {
-        return "Gymnastic(id=" + this.getId() + ", name=" + this.getName() + ", description=" + this.getDescription() + ")";
-    }
 }
