2174738ff4e9d6d81e5b1e9fce3a2f53e18d3cc5
Alexandr <Sanom1907@gmail.com>
2020-06-04 19:29:32 +0300

    deleteWrongTraining

--- src/main/java/com/sportproject/gym/DTO/SetEntityDTO.java
+++ src/main/java/com/sportproject/gym/DTO/SetEntityDTO.java
@@ -10,6 +10,7 @@ import lombok.Data;
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class SetEntityDTO {
 
+    private long id;
     private int repeats;
     private double weight;
 //    private long visitId;
